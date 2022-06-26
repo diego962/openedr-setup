fdb10ef7a7fdda095cae75c79a146f1afdefb2f5
diego962 <diegolopes962@gmail.com>
2022-06-26 15:09:29 -0300

    Adicionada funcionalidade para criar um profile para disponibilizar o atomic red team para todos os usuários do sistema

--- openedr_lab_setup.ps1
+++ openedr_lab_setup.ps1
@@ -1,11 +1,7 @@
 
 $User = $env:Username
 $PathEDRMsi = "C:\Users\$User\Downloads\OpenEDR-installation-2.0.0.0-x64.msi"
-
-
-function DisableVirusAndRealProtection {
-    Set-MpPreference -DisableRealtimeMonitoring $true
-}
+$PathProfile = $env:windir\System32\WindowsPowerShell\v1.0\atomicredteam.ps1
 
 function ChangeExecutionPolicy {
     Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
@@ -15,14 +11,20 @@ function InstallAtomicReadteam {
     IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics
 }
 
+function CreateProfile {
+    New-item -Path $PathProfile -ItemType file -Force
+
+    Add-Content $PathProfile "Import-Module C:\AtomicRedTeam\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1"
+}
+
 function InstallOpenEDR {
-    IWR -Uri 'https://github.com/ComodoSecurity/openedr/releases/download/2.0.0.0/OpenEDR-installation-2.0.0.0-x64.msi' -OutFile "$PathEDR"
+    IWR -Uri 'https://github.com/ComodoSecurity/openedr/releases/download/2.0.0.0/OpenEDR-installation-2.0.0.0-x64.msi' -OutFile $PathEDR
 
     Start-Process $PathEDRMsi
 }
 
 
-DisableVirusAndRealProtection
 ChangeExecutionPolicy
 InstallAtomicReadteam
+CreateProfile
 InstallOpenEDR
